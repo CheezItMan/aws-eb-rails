61728030521a219ef1e966d9f003842950b8b23f
Chris <chris@adadevelopersacademy.org>
2017-07-18 15:25:05 -0700

    Added lets encrypt message.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def letsencrypt
+    render plain: ENV['LE_AUTH_RESPONSE']
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
-
+  get "/.well-known/acme-challenge/#{ENV['LE_AUTH_REQUEST']}", to: 'welcome#letsencrypt'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
